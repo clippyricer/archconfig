3f7065dc514941409e3d154c5affce364d431b78
clippyricer <bannansmooth68@outlook.com>
2026-02-15 13:42:22 -0500

    add fonts

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ kittyDir="~/.config/kitty/"
 cp kitty/* ~/.config/kitty/
 cp zsh/p10k.zsh ~/.p10k.zsh
 cp zsh/zshrc ~/.zshrc
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+unzip JetBrainsMono.tar.xz && mkdir /usr/share/fonts/JetBrainsMono/
+mv *.ttf /usr/share/fonts/JetBrainsMono/
+rm README.md && rm OFL.txt
+fc-cache -frv && rm JetBrainsMono.tar.xz
 
 echo "Done!"
-echo "To see effects close your terminal and/or run source ~/.zshrc"
+echo "To see effects close your terminal and open up kitty to see the effects"
